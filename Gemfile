59536339e2ca765416be83c4a4fe04b2a59530bc
Anthony Duque <duquea95@gmail.com>
2016-10-14 11:46:38 -0400

    update

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'react-rails'
 gem 'bootstrap-sass', '~> 3.3.6'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem 'mail_form'
+gem 'simple_form'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -38,6 +40,7 @@ gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'foreman'
   gem 'byebug', platform: :mri
 end
 
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,20 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# 
+# config.action_mailer.smtp_settings = {
+#   address: "smtp.gmail.com",
+#   port: 587,
+#   domain: "<example.tld>",
+#   user_name: "<username>",
+#   password: "<password>",
+#   authentication: :plain,
+#   enable_starttls_auto: true
+# }
+#
+# config.action_mailer.default_url_options = {
+#   host: "yourdomain.tld"
+# }
 
 module Portfolio
   class Application < Rails::Application
