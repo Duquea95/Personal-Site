a33d5c4281b08dfe290ecdbbff8d4469d5735b86
Anthony Duque <duquea95@gmail.com>
2016-10-23 00:36:09 -0400

    change about me

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,7 +1,16 @@
 $(function() {
-  $h1 = $("h1");
+    $h1 = $("h1");
 
-  $h1.on("click",function(){
-      $h1.text("Good shit!");
-  })
-})
+    $h1.on("click",function(){
+        console.log(this.hasAttribute("data-type"));
+        // switch (this = true;) {
+        //     case expression:
+        //
+        //         break;
+        //
+        //     default:
+        //
+        // }
+    })
+
+});
