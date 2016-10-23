f68d1402f6f5e079c1810610bc3f4f5ff1df0d7f
Anthony Duque <duquea95@gmail.com>
2016-10-23 01:10:01 -0400

    add jquery

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,15 +2,44 @@ $(function() {
     $h1 = $("h1");
 
     $h1.on("click",function(){
-        console.log(this.hasAttribute("data-type"));
-        // switch (this = true;) {
-        //     case expression:
-        //
-        //         break;
-        //
-        //     default:
-        //
-        // }
+        console.log(this.dataset.type);
+
+        switch(this.dataset.type) {
+            case "service":
+                var datatype = this.dataset.type
+                $p = $("p").hidden
+
+                console.log(datatype);
+                break;
+
+            case "experience":
+                var datatype = this.dataset.type
+                $p = $("p").hidden
+
+                console.log(datatype);
+
+                // if($("p").dataset.type == "experience"){
+                //     console.log(this);
+                //     // this.toggle("hidden");
+                // };
+
+                break;
+
+            case "whoAmI":
+                var datatype = this.dataset.type
+                $p = $("p").hidden
+
+                console.log(datatype);
+                break;
+
+            case "extra":
+                var datatype = this.dataset.type
+                $p = $("p").hidden
+
+                console.log(datatype);
+                break;
+
+        }
     })
 
 });
